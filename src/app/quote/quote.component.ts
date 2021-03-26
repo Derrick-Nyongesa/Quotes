21659b653a2ef53b9a81ef3bac0bc085743436b1
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-03-26 10:09:58 +0300

    added more quotes to the array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,14 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Derrick', 'Many of life’s failures are people who did not realize how close they were to success when they gave up.', 'Thomas A. Edison', new Date(2021,2,25)),
-    new Quote(2, 'Daniel', 'Life is what happens when you’re busy making other plans.', 'John Lennon', new Date(2021,2,27)),
-    new Quote(3, 'Maria', 'The purpose of our lives is to be happy.', 'Dalai Lama', new Date(2021,2,22)),
+    new Quote(1, 'Derrick', 'The way I see it, every life is a pile of good things and bad things. The good things don’t always soften the bad things, but vice versa, the bad things don’t always spoil the good things and make them unimportant.', 'Doctor Who', new Date(2021,2,25)),
+    new Quote(2, 'Daniel', 'My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.', 'Maya Angelou', new Date(2021,2,27)),
+    new Quote(3, 'Maria', 'There’s more to life than basketball. The most important thing is your family and taking care of each other and loving each other no matter what.', 'Stephen Curry', new Date(2021,2,22)),
     new Quote(4, 'Nico', 'Get busy living or get busy dying.', 'Stephen King', new Date(2021,1,18)),
     new Quote(5, 'Vanessa', 'Many of life’s failures are people who did not realize how close they were to success when they gave up.', 'Thomas A. Edison', new Date(2021,0,7)),
+    new Quote(6, 'Emenalo', 'You can’t put a limit on anything. The more you dream, the farther you get.', 'Michael Phelps', new Date(2021,0,21)),
+    new Quote(7, 'Ciku', 'I believe you make your day. You make your life. So much of it is all perception, and this is the form that I built for myself. I have to accept it and work within those compounds, and it’s up to me.', 'Brad Pitt', new Date(2021,1,4)),
+    new Quote(8, 'Waiyaki', 'IA lot of people give up just before they’re about to make it. You know you never know when that next obstacle is going to be the last one.', 'Cuck Norris', new Date(2021,2,3)),
   ];
 
   toggleQuote(index){
